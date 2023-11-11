c660e6b691c79840c90805ca0ee01d24702c4ea5
Adam Janicki <adamjanicki2@gmail.com>
2023-11-10 20:56:17 -0500

    update about and upload

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,7 +1,5 @@
 import usePageTitle from "src/hooks/usePageTitle";
 import "src/components/pages/home.css";
-import Link from "src/components/basic/Link";
-import Button from "src/components/basic/Button";
 
 const Home = () => {
   usePageTitle("FirmEye");
@@ -9,17 +7,10 @@ const Home = () => {
     <div className="bg-image home-marquee flex flex-column items-center justify-between">
       <div className="mt5 w-100 ph3 flex flex-column items-center">
         <h1 className="marquee-text fw8 tc mb0 white">FirmEye</h1>
-        <p className="pa0 pb2 fw4 f4 light-gray">
-          Your analysis tool for any business need.
+        <p className="pa0 pb2 fw4 f4 light-gray tc">
+          Your automated tool that researches, formats, and extrapolates
+          business data.
         </p>
-        <div className="flex items-center">
-          <Link to="/about" unstyled className="mr3">
-            <Button inverse>Learn more</Button>
-          </Link>
-          <Link to="/upload" unstyled>
-            <Button inverse>Upload</Button>
-          </Link>
-        </div>
       </div>
     </div>
   );
--- src/components/pages/Upload.tsx
+++ src/components/pages/Upload.tsx
@@ -2,8 +2,20 @@ import Page from "src/components/modules/Page";
 
 const Upload = () => {
   return (
-    <Page title="Upload">
-      <p>TODO: Add google form iframe here</p>
+    <Page documentTitle="Upload" title="Submit businesses for processing">
+      <iframe
+        title="Upload Form"
+        src="https://docs.google.com/forms/d/e/1FAIpQLScKPdalj7vW3US1mx6SW5qWSO9iHvmEyB56avUK3oB_oXXb7Q/viewform?embedded=true"
+        height="1200"
+        width="100%"
+        style={{
+          border: "none",
+          outline: "none",
+          flexGrow: 1,
+        }}
+      >
+        Loading…
+      </iframe>
     </Page>
   );
 };
