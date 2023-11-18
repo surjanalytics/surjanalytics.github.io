9c13100d88e7801fc50d23b6a5eb6a02fe4ded06
Adam Janicki <adamjanicki2@gmail.com>
2023-11-18 15:32:30 -0500

    new homepage

--- src/components/modules/Footer.tsx
+++
@@ -1,7 +0,0 @@
-const Footer = () => (
-  <footer className="pa4 flex items-center justify-center w-100 bt b--moon-gray bw1">
-    <p className="fw5 f5">&copy; 2023 Surjanalytics</p>
-  </footer>
-);
-
-export default Footer;
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// Make sure to import this first so your styles take priority!
 import "tachyons/css/tachyons.min.css";
-// All your styles go here!
-import "src/css/style.css";
-import "src/css/media.css";
-import App from "src/App";
+import "src/style.css";
+import "src/media.css";
+import App from "src/components/App";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
